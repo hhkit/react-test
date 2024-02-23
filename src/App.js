67651abe0cf528a7ff020be157bfa9274d400843
Ivan <hohkivan@gmail.com>
2024-02-23 13:53:55 +0800

    eslint setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+
+import logo from './logo.svg';
+
+
+/**
+ * @return {ReactElement} React componen
+ */
 function App() {
   return (
     <div className="App">
